1f528a5d4bdb3c2b07c7d125a1392a2f0838ffb5
Wayne Allan <wayne.allan@thoughtworks.com>
2025-07-27 20:36:16 +1000

    Remove font imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
